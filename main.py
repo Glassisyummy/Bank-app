a19f6f7990f74f8b1f97048313e175272048f975
Glassisyummy <mia.gutierrez537@gmail.com>
2023-04-20 18:24:17 -0500

    creating tkinter menu screen

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
 from tkinter import *
+import tkinter
+
+def bank_label():
+    label = customtkinter.CTkLabel(value="Welcome to Honesty Banks!")
+    label.place(relx = 0.5, rely = 0.5, anchor = tkinter.NW)
